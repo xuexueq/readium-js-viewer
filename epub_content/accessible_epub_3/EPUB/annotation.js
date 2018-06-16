37ad9581f4be51005a5b9099f6cad0656cc450ea
Xue QiaoLing <i-xueqiaoling@mobike.com>
2018-06-16 18:26:55 +0800

    annotation

--- epub_content/accessible_epub_3/EPUB/annotation.js
+++ epub_content/accessible_epub_3/EPUB/annotation.js
@@ -1,17 +1,25 @@
 console.log(1234321);
+let selectionDOM;
+let selectionText;
 function sinaMiniBlogShare(eleShare, eleContainer) {
 	var eleTitle = document.getElementsByTagName("title")[0];
-	eleContainer = eleContainer || document;
+	eleContainer = eleContainer || document.getElementsByTagName('section')[0];
 	var funGetSelectTxt = function() {
 		var txt = "";
 		if(document.selection) {
-			txt = document.selection.createRange().text;    // IE
+			selectionDOM = document.selection.createRange();
+			txt = selectionDOM.text;    // IE
 		} else {
-			txt = document.getSelection();
+			selectionDOM = document.getSelection();
+			txt = selectionDOM;
 		}
-		return txt.toString();
+		console.log('selectionDOM111111111111111',selectionDOM)
+		console.log('22222',JSON.parse(JSON.stringify(selectionDOM)))
+		selectionText = txt.toString();
+		return selectionText;
 	};
 	eleContainer.onmouseup = function(e) {
+		console.log('onmouseup')
 		e = e || window.event;
 		var txt = funGetSelectTxt(), sh = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
 		var left = (e.clientX - 40 < 0) ? e.clientX + 20 : e.clientX - 40, top = (e.clientY - 40 < 0) ? e.clientY + sh + 20 : e.clientY + sh - 40;
@@ -27,21 +35,50 @@ function sinaMiniBlogShare(eleShare, eleContainer) {
 			eleShare.style.display = "none";
 		}
 	};
-	eleShare.onclick = function() {
-		var txt = funGetSelectTxt();
+	document.getElementById('annotation-btn').onclick = function() {
+		var txt = selectionText;
 		console.log(txt,'===')
 		eleShare.style.display = "none";
 		document.getElementById("annotation-mask").style.display = "inline";
 		document.getElementById("annotation-input").style.right = "10px";
 	};
+
+	document.getElementById('highlight-btn').onclick = function() {
+		var range = selectionDOM.getRangeAt(0);
+		var span = document.createElement('span');
+		span.style.backgroundColor = '#fbfb41';
+		range.surroundContents(span);
+		eleShare.style.display = "none";
+	}
 }
 
-document.getElementById('cancel').onclick = function () {
+document.getElementById('cancel').onmouseup = function(e) {
+	e.preventDefault();
+	e.stopPropagation();
+}
+document.getElementById('confirm').onmouseup = function(e) {
+	e.preventDefault();
+	e.stopPropagation();
+}
+document.getElementById('cancel').onclick = function (e) {
+	e.preventDefault();
+	e.stopPropagation();
 	document.getElementById("annotation-mask").style.display = "none";
 	document.getElementById("annotation-input").style.right = "-300px";
 }
 
-document.getElementById('confirm').onclick = function () {
+
+document.getElementById('confirm').onclick = function (e) {
+	e.preventDefault();
+	e.stopPropagation();
+	var range = selectionDOM.getRangeAt(0);
+	var span = document.createElement('span');
+	span.style.backgroundColor = 'pink';
+	range.surroundContents(span);
+
+	console.log('selectionText',selectionText)
+	console.log('selectionDOM',selectionDOM)
+	selectionDOM.innerHTML=`<span>1231</span>`;
 	document.getElementById("annotation-mask").style.display = "none";
 	document.getElementById("annotation-input").style.right = "-300px";
 }
